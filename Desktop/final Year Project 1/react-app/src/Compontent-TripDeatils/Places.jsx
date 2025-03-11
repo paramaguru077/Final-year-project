cbed95dccbb9d1982dac1bccb8d295e08ed34940
paramaguru077 <gurucse077@gmail.com>
2025-03-11 17:46:44 +0530

    added some feture

--- Desktop/final Year Project 1/react-app/src/Compontent-TripDeatils/Places.jsx
+++ Desktop/final Year Project 1/react-app/src/Compontent-TripDeatils/Places.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 
 const Places = ({trip}) => {
- // console.log(trip);
-  //console.log(Object.keys(trip.tripData.itinerary).sort((a,b)=> 
-  //Number(a.replace("day",""))-Number(b.replace("day",""))));
+ if(!trip || !trip.tripData || !trip.tripData.itinerary){
+  return <p className='text-center text-xl text-red-500'> No trip data available</p>
+ }
+ const sortedDays = Object.keys(trip.tripData.itinerary).sort(
+  (a, b) => Number(b.replace("day", "")) - Number(a.replace("day", ""))
+)
+const sorteDays = sortedDays.map((day)=>{
+  const dayData = trip?.tripData?.itinerary[day];
+  return {
+    day,
+    theme:dayData?.theme,
+    bestTimeToVisit:dayData?.bestTimeToVisit,
+    activities:dayData?.activities.map((activity)=>(
+      {
+        placeName:activity?.placeName,
+        placeDetails:activity?.placeDetails||"No details Available",
+        ticketPricing:activity?.ticketPricing,
+        travelTime:activity?.travelTimeBetweenLocations,
+        
+      }
+
+    )) 
+   }
+})
+ 
+ console.log(sortedDays)
+ //console.log(Object.keys(trip?.tripData?.itinerary))
  
   return (
     <div className='max-w-[1000px] md:mx-auto mx-24 mt-10'>
       <h1 className='text-center text-2xl lg:text-4xl font-bold drop-shadow-lg text-neutral-700'>Places</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 p-2 mt-7'>
+
         
 
 
